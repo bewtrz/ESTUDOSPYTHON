2eb94945ab5d567e894bd9a74192ce18f33be209
unknown <SENAI@C303-SALA4-029.senaiescola.fsa>
2023-08-16 11:31:29 -0300

    estudospython

---
+++ dicionario&tuplas.py
@@ -0,0 +1,42 @@
+'''TUPLAS - 
+é uma lista, mas que são imutavéis (os dados nela não podem ser alterados). Geralmente são usadas
+para armazenar dados que não mudam tão fácil, como estações ano, meses, vogais e etc.
+
+Usada para armazenar itens heterogêneos (que não se misturam) 
+Oferece estrutura para os dados
+Indicadas entre (parênteses) 
+
+Ela é indicada para armazenar dados que não vão ser alterados.
+
+vantagens: por os dados armazenados serem imutaveis, ela traz uma segurança maior para o 
+código.
+
+tupla = ('beatriz', 10, 'joão')
+print(tupla)
+
+'''
+
+'''
+DICIONARIO - básicamente é uma lista que armazenar dados com chave e seu valor correspondente.
+Para iniciar um dicionario se usa {chaves}.
+
+telefones = {'beatriz': 75991243467}
+print(telefones['beatriz'])
+
+'''
+
+senai_coordenadas = (-23.588254, -46.632477)
+id(senai_coordenadas)
+senai_coordenadas += (-5,)
+print(senai_coordenadas)
+
+
+#print(type(senai_coordenadas))
+
+
+nomes = ('beatrz', 'alisson','gabriel')
+sobrenomes = ('rodrigues','costa', 'nascimento')
+
+
+
+
---
+++ exercicioDicionario.py
@@ -0,0 +1,56 @@
+
+#QUESTÃO - 01 
+#Crie um dicionário d e coloque nele seus dados: nome, idade,telefone,endereço. Usando o dicionário d criado anteriormente, imprima seu nome. 
+
+
+#dados = {'nome': 'beatriz', 'idade': 24, 'telefone': 75991243467, 'endereco': 'rua nescolandia, Feira de santana'} 
+#print(dados['nome'])
+
+
+#QUESTÃO - 02 
+#Crie um dicionário d e coloque nele os dados fornecidos pelo usuário:nome, idade, telefone,endereço. 
+#Também usando d, imprima todos os itens do dicionário no formato chave : valor, ordenado pela chave
+
+#dados2 = {}
+
+# dados2['nome'] = input('digite seu nome: ')
+# dados2['idade'] = input('digite sua idade: ')
+# dados2['telefone'] = input('digite seu telefone: ')
+# dados2['endereco'] = input('digite seu endereço:: ')
+# print(dados2)
+
+#QUESTÃO - 03 
+#Crie um dicionário que é uma agenda e coloque nele os seguintes dados:chave (cpf), nome, idade, telefone. 
+# O programa deve ler um número indeterminado de dados, criar a agenda e imprimir todos os itens do dicionário no formato chave: nome-idadefone.
+
+Agenda = {}
+
+Agenda['cpf'] = input('Digite seu cpf: ')
+Agenda['nome'] = input('Digite seu nome: ')
+Agenda['idade'] = input('Digite sua idade: ')
+Agenda['telefone'] = input('Digite seu telefone: ')
+
+if 'cpf' == 'cpf':
+    print('nome: ',Agenda['nome'])
+    print('idade: ',Agenda['idade'])
+    print('telefone: ',Agenda['telefone'])
+
+
+
+    
+
+
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
